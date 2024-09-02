5201f6ddb8ff7f4806b3d4b3c4649986b4636e24
Jenny Fang <jfang5792@gmail.com>
2024-09-01 21:41:14 -0700

    updated code

--- markov.py
+++ markov.py
@@ -1,4 +1,4 @@
-"""A Markov chain generator that can tweet random messages."""
+"""A Markov chain generator that can respond with song excerpts."""
 import os
 import discord
 import sys
@@ -23,6 +23,7 @@ def make_chains(text_string):
     chains = {}
 
     words = text_string.split()
+    # words.append(None)
     for i in range(len(words) - 2):
         key = (words[i], words[i + 1])
         value = words[i + 2]
@@ -46,9 +47,6 @@ def make_text(chains):
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text).
 
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
-
         word = choice(chains[key])
         words.append(word)
         key = (key[1], word)
@@ -57,17 +55,19 @@ def make_text(chains):
 
 
 # Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
+# python markov.py cherry.txt
+# python markov.py the-one.txt
+# python markov.py tears-ricochet.txt
 filenames = sys.argv[1:]
 
 if not filenames:
-    print("Please provide a filename on the command line! Ex. python3 markov.py green-eggs.txt")
+    print("Please provide a filename on the command line! Ex. python3 markov.py cherry.txt")
     exit(1)
 
 # Open the files and turn them into one long string
 text = open_and_read_file(filenames)
 
-# Get a Markov chain
+# Get Markov chain
 chains = make_chains(text)
 response = make_text(chains)
 
@@ -85,8 +85,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # TODO: replace this with your code
-    if message.content.startswith('hi'):
+    if message.content.startswith('gimme song lyrics from it ends with us soundtrack'):
         await message.channel.send(make_text(chains))
 
 #client.run('')
